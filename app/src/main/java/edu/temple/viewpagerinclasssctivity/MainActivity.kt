58826b90704cf45ecc25906166d1eb5bde4c3649
Eric Reizas <tuo17224@temple.edu>
2024-03-28 11:19:25 -0400

    Attached TextFragment to Main Activity

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView, TextFragment.newInstance("Hi"))
+            .commit()
     }
 
 }
